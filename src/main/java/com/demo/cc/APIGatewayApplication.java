111faaa56e24aea119d1229678dff0a80ae70358
chanduujjina129 <ujjinachnadrasekhar@gmail.com>
2025-10-18 12:53:42 +0530

    API Gate way configurations

---
+++ src/main/java/com/demo/cc/APIGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.demo.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APIGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APIGatewayApplication.class, args);
+	}
+
+}
